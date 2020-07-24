5722a03e875d243be96589a82f113c127c18c077
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-07-24 16:27:40 +0300

    Update task-01.js

--- task-01.js
+++ task-01.js
@@ -1,10 +1,34 @@
 https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript?fbclid=IwAR1RjHO-v2IKkBWtIiMQm_g3lkNysCq8lXTMAS1BVHTgQGRWbanCfaqrtoU
 
 function greet (name, owner) {
- if(name == owner){
+  if(name == owner) {
    return "Hello boss";
  }
-  else{
+  else {
     return "Hello guest";
   }
 }
+
+
+https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript?fbclid=IwAR0e9MwaRbdxb--DFIpW3B6wX7TkMVdqkhUsl-HPSdW6dUsiFyuH577_bFo
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+ return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+
+https://www.codewars.com/kata/keep-hydrated-1/train/javascript?fbclid=IwAR1gHze2iEy02RvPbaKuPvFetDQvm7cR79zauDuCQCu8RaQzbcwb8zTTt2c
+
+function litres(time) {
+  return Math.floor(time / 2);
+}
+
+
+https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript?fbclid=IwAR0UU_PUgmE37om1lQyKH3neYzt4W9zMdy5uwaqk0HdQopPjKObwx9W4kkQ
+
+function move (position, roll) {
+  return (roll * 2) + position;
+}
+
+
+
