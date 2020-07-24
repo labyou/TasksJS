ec6af862851fb3a203ccae2849e48f4130d624a8
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-07-24 11:43:19 +0300

    Create task-01.js

---
+++ task-01.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript?fbclid=IwAR1RjHO-v2IKkBWtIiMQm_g3lkNysCq8lXTMAS1BVHTgQGRWbanCfaqrtoU
+
+function greet (name, owner) {
+ if(name == owner){
+   return "Hello boss";
+ }
+  else{
+    return "Hello guest";
+  }
+}
